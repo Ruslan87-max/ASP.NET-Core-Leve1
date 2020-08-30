48b211128da770fe5ea8dccdaeb428e3bf0be1a5
Руслан Руслан <rus87mukh@mail.ru>
2020-08-30 13:50:41 +0300

    Initial commit

---
+++ Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lesson1_Task1_MiddleWare.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public string Univercity { get; set; } 
+        public string FacultyName { get; set; } 
+        public string Date { get; set; } //Дата поступления
+    }
+}
---
+++ Models/StudentsList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lesson1_Task1_MiddleWare.Models
+{
+    public class StudentsList
+    {
+        public List<Student> students = new List<Student>();
+
+        public StudentsList()
+        {
+            students.Add(new Student() { Id = 1, Name = "Владимир", SurName = "Иванов", Univercity = "МГУ", FacultyName = "Факультет психологии", Date = "1.09.2009" });
+            students.Add(new Student() { Id = 2, Name = "Ирина", SurName = "Петрова", Univercity = "МИРЭА", FacultyName = "Информатика и вычислительная техника", Date = "1.09.2002" });
+            students.Add(new Student() { Id = 3, Name = "Владимир", SurName = "Иванов", Univercity = "МИРЭА", FacultyName = "Программная инженерия", Date = "1.09.2015" });
+        }
+    }
+}
