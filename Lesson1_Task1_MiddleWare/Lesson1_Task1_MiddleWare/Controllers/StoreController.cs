60d1349684f948671c51703e4c850e4904b6d36a
Руслан Руслан <rus87mukh@mail.ru>
2020-08-31 21:29:11 +0300

    Добваление мастера-страниц _Layout.cshtml

---
+++ Lesson1_Task1_MiddleWare/Lesson1_Task1_MiddleWare/Controllers/StoreController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lesson1_Task1_MiddleWare.Controllers
+{
+    public class StoreController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Blog()
+        {
+            return View();
+        }
+
+        public IActionResult BlogSingle()
+        {
+            return View();
+        }
+        public IActionResult Cart()
+        {
+            return View();
+        }
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+        public IActionResult ContactUs()
+        {
+            return View();
+
+        }
+        public IActionResult NotFound404()
+        {
+            return View();
+
+        }
+        public IActionResult ProductDetails()
+        {
+            return View();
+
+        }
+        public IActionResult Shop()
+        {
+            return View();
+
+        }
+    }
+}
