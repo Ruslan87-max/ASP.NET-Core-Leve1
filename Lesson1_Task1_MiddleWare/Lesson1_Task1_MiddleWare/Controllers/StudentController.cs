7dd70a30086d347da8301162f7ae3a0faf47ae5e
Руслан Руслан <rus87mukh@mail.ru>
2020-09-03 21:19:22 +0300

    Реализован интерфейс для работы со студентами, а именно: возможность редактирования и сохранения, удаления, добавления в список.

--- Lesson1_Task1_MiddleWare/Lesson1_Task1_MiddleWare/Controllers/StudentController.cs
+++ Lesson1_Task1_MiddleWare/Lesson1_Task1_MiddleWare/Controllers/StudentController.cs
@@ -2,23 +2,116 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Lesson1_Task1_MiddleWare.InfroStrucure;
 using Lesson1_Task1_MiddleWare.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Lesson1_Task1_MiddleWare.Controllers
 {
-    public class StudentController : Controller 
+    [Route("Users")]
+    public class StudentController : Controller
     {
-        StudentsList studentsList = new StudentsList();
-        public IActionResult Index1()
+        private readonly IStudentsService _studentsService;
+        public StudentController(IStudentsService studentsService)
         {
-            return View(studentsList.students);
+            _studentsService = studentsService;
         }
 
-        public IActionResult StudentDetails1(int id)
+        StudentsList studentsList = new StudentsList();
+
+        [Route("all")]
+        public IActionResult Index()
         {
-            var _students = studentsList.students.FirstOrDefault(x => x.Id.Equals(id));
-            return View(_students == null ? null : _students);
+            return View(_studentsService.GetAll());
+        }
+
+        [Route("{id?}")]
+        public IActionResult StudentDetails(int id)
+        {
+            //var _students = studentsList.students.FirstOrDefault(x => x.Id.Equals(id));
+            //return View(student == null ? null : _students);
+            var student = _studentsService.GetById(id);
+
+            if (ReferenceEquals(student, null))
+                return NotFound();//возвращаем результат 404 Not Found
+
+            //Иначе возвращаем сотрудника
+            return View(student);
+        }
+
+        [HttpGet]
+        [Route("edit/{id?}")]
+        public IActionResult Edit(int? id)
+        {
+            Student studentModel;
+            if (id.HasValue)
+            {
+                studentModel = _studentsService.GetById(id.Value);
+                if (ReferenceEquals(studentModel, null))
+                    return NotFound();// возвращаем результат 404 Not Found
+            }
+            else
+            {
+                studentModel = new Student();
+            }
+            return View(studentModel);
+
+        }
+
+        [HttpPost]
+        [Route("edit/{id?}")]
+        public IActionResult Edit(Student studentModel)
+        {
+            if (studentModel.Id > 0)
+            {
+                var dbItem = _studentsService.GetById(studentModel.Id);
+
+                if (ReferenceEquals(dbItem, null))
+                    return NotFound();// возвращаем результат 404 Not Found
+
+                dbItem.Name = studentModel.Name;
+                dbItem.SurName = studentModel.SurName;
+                dbItem.Univercity = studentModel.Univercity;
+                dbItem.FacultyName = studentModel.FacultyName;
+                dbItem.Date = studentModel.Date;
+            }
+            else
+            {
+                _studentsService.AddNew(studentModel);
+            }
+
+            _studentsService.Commit();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        [Route("delete/{id?}")]
+        public IActionResult Delete(int? id) 
+        {
+            if (id.Value > 0)
+            {
+                var dbItem = _studentsService.GetById(id.Value);
+                if (dbItem != null)
+                    return View(dbItem);
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        [Route("delete/{id?}")]
+        public IActionResult Delete(Student studentModel)
+        {    
+            if (studentModel.Id > 0)
+            {
+                var dbItem = _studentsService.GetById(studentModel.Id);
+                if (dbItem != null)
+                {
+                    _studentsService.Delete(studentModel.Id);
+                    return RedirectToAction("Index");
+                }
+            }
+            return NotFound();
         }
     }
 }
--- Lesson1_Task1_MiddleWare/Lesson1_Task1_MiddleWare/Models/StudentsList.cs
+++ Lesson1_Task1_MiddleWare/Lesson1_Task1_MiddleWare/Models/StudentsList.cs
@@ -13,7 +13,7 @@ namespace Lesson1_Task1_MiddleWare.Models
         {
             students.Add(new Student() { Id = 1, Name = "Владимир", SurName = "Иванов", Univercity = "МГУ", FacultyName = "Факультет психологии", Date = "1.09.2009" });
             students.Add(new Student() { Id = 2, Name = "Ирина", SurName = "Петрова", Univercity = "МИРЭА", FacultyName = "Информатика и вычислительная техника", Date = "1.09.2002" });
-            students.Add(new Student() { Id = 3, Name = "Владимир", SurName = "Иванов", Univercity = "МИРЭА", FacultyName = "Программная инженерия", Date = "1.09.2015" });
+            students.Add(new Student() { Id = 3, Name = "Антон", SurName = "Поляков", Univercity = "МИРЭА", FacultyName = "Программная инженерия", Date = "1.09.2015" });
         }
     }
 }
